957b4398c21fe9adc5c79d831ce0ef2275e6d1ec
Gaurav <giganticgemmic@gmail.com>
2016-07-23 03:44:05 +0530

    Add files via upload

---
+++ start.py
@@ -0,0 +1,29 @@
+YOUR-PHONE-NUMBER = raw_input("WhatsappLogin -----> ")
+fo = open("YOUR-PHONE-NUMBER.txt","w")
+fo.write(YOUR-PHONE-NUMBER)
+fo.close()
+
+PASSWORD= raw_input("WhatsappPassword -----> ")
+fo = open("PASSWORD.txt","w")
+fo.write(PASSWORD)
+fo.close()
+
+WAY2SMS-LOGIN= raw_input("Way2SMSLogin -----> ")
+fo = open("WAY2SMS-LOGIN= .txt","w")
+fo.write(WAY2SMS-LOGIN= )
+fo.close()
+
+WAY2SMS-PASSWORD= raw_input("Way2SMSPassword -----> ")
+fo = open("WAY2SMS-PASSWORD.txt","w")
+fo.write(WAY2SMS-PASSWORD)
+fo.close()
+
+GROUP-NAME= raw_input("groupname-----> ")
+fo = open("GROUP-NAME.txt","w")
+fo.write(GROUP-NAME)
+fo.close()
+
+KEYWORD= raw_input("Keyword------> ")
+fo = open("KEYWORD.txt","w")
+fo.write(KEYWORD)
+fo.close()
